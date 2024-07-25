65d44851dbe66e5d524ca364b3ec83eda4f11b5d
lijunlin <lijunlin2022@gmail.com>
2024-07-25 08:25:11 +0800

    feat: 设置字体、字体大小

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,19 @@
-import { View, Text, Button } from '@tarojs/components'
+import { View, Button, Picker } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import ePub from '../../epubjs/src'
 
 export default function Index() {
   const renditionRef = useRef()
+  const [fontSizeRange] = useState([14, 16, 18])
+  const [currentFontSize, setCurrentFontSize] = useState(14)
+  const [fontFamilyRange] = useState([
+    'Microsoft Yahei',
+    'KaiTi',
+    'FangSong'
+  ])
+  const [currentFontFamily, setCurrentFontFamily] = useState('Microsoft Yahei')
 
   useLoad(() => {
     console.log('Page loaded.')
@@ -29,12 +37,31 @@ export default function Index() {
     renditionRef.current.next()
   }
 
+  const changeFontSize = (e) => {
+    const index = e.detail.value
+    const size = fontSizeRange[index]
+    setCurrentFontSize(size)
+    renditionRef.current.themes.fontSize(size)
+  }
+
+  const changeFontFamily = (e) => {
+    const index = e.detail.value
+    const family = fontFamilyRange[index]
+    setCurrentFontFamily(family)
+    renditionRef.current.themes.font(family)
+  }
+
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
       <View id='area'></View>
       <Button onClick={clickPrev}>prev</Button>
       <Button onClick={clickNext}>next</Button>
+      <Picker mode='selector' range={fontSizeRange} onChange={changeFontSize}>
+        <View>字号: {currentFontSize}</View>
+      </Picker>
+      <Picker mode='selector' range={fontFamilyRange} onChange={changeFontFamily}>
+        <View>字体: {currentFontFamily}</View>
+      </Picker>
     </View>
   )
 }
